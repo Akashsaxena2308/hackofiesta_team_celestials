f3dfe889e0075d9e81c5e5d586bd1bd2ecb16542
Akashsaxena2308 <lordmartin2308@gmail.com>
2021-04-18 07:38:44 +0530

    Minor Changes

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -10,13 +10,13 @@ const LandingScreen = () => {
       <div className="row">
         <div className="col-lg-4">
           <div className="md-5 Heading">
-         <h2 style={{color: 'white', fontWeight: 'bolder',margin: '20px',fontSize: '3rem'}}>Intract&Learn</h2>
+         <h2 style={{color: 'white', fontWeight: 'bolder',margin: '20px',fontSize: '3rem'}}>Interact&Learn</h2>
       </div>
       <div class="timeline-learning">
    <div class="line">
      <div class="line-content">
        <div class="content">
-         <h1>Intractive Learning</h1>
+         <h1>Interactive Learning</h1>
        </div>
 
        <div class="content">
